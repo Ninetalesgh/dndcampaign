ae351ba9c61a5cec35b7d82b4c41e148b9bc69da
Ninetalesgh <camillolukesch@gmail.com>
2025-01-05 00:42:32 +0100

    update dm

--- push_main_merge_dm.sh
+++ push_main_merge_dm.sh
@@ -4,17 +4,23 @@ echo == STASH
 git stash
 echo == CHECKOUT MAIN
 git checkout main
-echo == POP
+echo == STASH POP
 git stash pop
 echo == RM VAULT/DM/
 git rm *vault/dm/*
 echo == COMMIT
-git commit -m "update"
+git commit -m "update main"
 echo == PUSH
 git push
 echo == CHECKOUT DM
 git checkout dm
 echo == MERGE MAIN
 git merge main
-echo == POP
+echo == STASH POP
 git stash pop
+echo == ADD ALL
+git add -A
+echo == COMMIT
+git commit -m "update dm"
+echo == PUSH
+git push
