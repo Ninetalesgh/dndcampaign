0149c09c6492b0667af0d9deec8d6d904840f97a
Ninetalesgh <camillolukesch@gmail.com>
2025-01-05 13:51:55 +0100

    update

---
+++ push.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+printf "\n== STASH\n"
+git stash
+printf "\n== CHECKOUT MAIN\n"
+git checkout main
+printf "\n== STASH APPLY\n"
+git stash apply
+printf "\n== RM VAULT/DM/\n"
+git rm -r *vault/dm/*
+printf "\n== ADD ALL\n"
+git add -A
+printf "\n== COMMIT\n"
+git commit -m "update"
+printf "\n== PUSH\n"
+git push
+printf "\n== CHECKOUT DM\n"
+git checkout dm
+printf "\n== MERGE MAIN\n"
+git merge main
+printf "\n== STASH POP\n"
+git stash pop
+printf "\n== ADD ALL\n"
+git add -A
+printf "\n== COMMIT\n"
+git commit -m "update dm"
+printf "\n== PUSH\n"
+git push
