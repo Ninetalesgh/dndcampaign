e7a8fd31f2f3237f3e14728e89765063de57345e
Ninetalesgh <camillolukesch@gmail.com>
2025-04-30 20:44:54 +0200

    update

--- merge.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-printf "\n== CHECKOUT MAIN\n"
-git checkout dm
-git add -A
-printf "\n== COMMIT\n"
-git commit -m "merge from main"
-printf "\n== PUSH\n"
-git push
--- pull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# git checkout main
-# git pull
-# git checkout dm
-# git pull
-# git merge main --no-edit
-git pull
